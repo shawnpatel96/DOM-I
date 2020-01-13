055443986fa907959f82954987e51790c36856ba
shawn <shawnpatel196@gmail.com>
2020-01-13 15:51:26 -0600

    finished stretch timer

---
+++ stretch-assignment/digital_timer/index.js
@@ -0,0 +1,40 @@
+let seconds=0;
+let minutes=0;
+let hours=0;
+
+
+
+
+// stopwatch code here
+function stopWatch(){
+    seconds++;
+
+    // SECOND NUMBER RESET
+    if (seconds/60===1){
+        seconds=0;
+        minutes++;
+        // MIN NUMBER RESET
+        if(minutes/60===1){
+            minutes =0;    //resets mins to 0
+            hours++;    // increment hour by 1
+        }
+    }
+    // AT 10 SECONDS SET COLOR OF DIGITS TO RED
+    if(seconds ===10){
+        let redColor= document.getElementById("digits");
+        redColor.style.color="red"
+    }
+    // SECONDS RESET AT 10
+    if(seconds===11){     
+        seconds=0;
+        // AT 0 SECONDS SET COLOR BACK TO BLACK( there's probably a better way to do this but idk)
+        let redColor= document.getElementById("digits");
+        redColor.style.color="black"
+        
+    }
+
+    // display timer which will update 
+document.getElementById("digits").innerHTML= hours + ":"+ minutes + ":"+ seconds;
+}
+
+window.setInterval(stopWatch, 1000)
